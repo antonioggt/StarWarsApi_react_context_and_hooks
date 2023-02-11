efc18d5fcee6e62d4f99624d50e7ffb77a9d49d1
antonioggt <antonioggt85@gmail.com>
2023-02-10 22:18:05 -0300

    implementa multiplos filtros numéricos e seus botões de remoção

--- src/components/NumericFilter.jsx
+++ src/components/NumericFilter.jsx
@@ -1,18 +1,28 @@
 import React, { useContext, useEffect, useState } from 'react';
 import FilterContext from '../context/FilterContext';
 
+const typeOptions = ['population',
+  'orbital_period',
+  'diameter',
+  'rotation_period',
+  'surface_water'];
+
 function NumericFilter() {
-  const { setFirstFilter, firstFilter } = useContext(FilterContext);
-  const [previState, setPreviState] = useState([]);
+  const { setFirstFilter, firstFilter, dinamicFilter } = useContext(FilterContext);
+  // const [previState, setPreviState] = useState([]);
+  const [typeState, setTypeState] = useState(typeOptions);
   const [typeSelect, setTypeSelect] = useState('population');
   const [operatorSelect, setOperatorSelect] = useState('maior que');
   const [numberInput, setNumberInput] = useState(0);
+  const [multipleFilters, setMultiplesFilters] = useState([]);
+  const [accFilters, setAccFilters] = useState([]);
+  const [filterIds, setFilterIds] = useState(0);
 
-  useEffect(() => {
+  /* useEffect(() => {
     if (previState.length === 0) {
       setPreviState(firstFilter);
     }
-  }, [previState.length, firstFilter]);
+  }, [previState.length/* firstFilter ]); */
 
   function handleTypeSelect({ target }) {
     setTypeSelect(target.value);
@@ -26,22 +36,128 @@ function NumericFilter() {
     setNumberInput(target.value);
   }
 
+  function handleInitialSearch() {
+    const firstComp = typeSelect === 'population';
+    const secondComp = operatorSelect === 'maior que';
+    const thirdComp = +numberInput === 0;
+    if (firstComp && secondComp && thirdComp) {
+      setFirstFilter(firstFilter.filter((el) => el[typeSelect] !== 'unknown'));
+      return true;
+    }
+    return false;
+  }
+
   function handleNumericChanges() {
-    console.log(previState.filter((el) => +el[typeSelect] < +numberInput));
+    /* console.log(previState.filter((el) => +el[typeSelect] < +numberInput)); */
     switch (operatorSelect) {
     case 'maior que':
-      setFirstFilter(previState.filter((el) => +el[typeSelect] > +numberInput));
+      setFirstFilter(firstFilter.filter((el) => +el[typeSelect] > +numberInput));
+      setAccFilters([...accFilters,
+        {
+          typeSelect,
+          operatorSelect,
+          numberInput,
+          id: filterIds + 1,
+        }]);
+      setFilterIds(filterIds + 1);
       break;
     case 'igual a':
-      setFirstFilter(previState.filter((el) => +el[typeSelect] === +numberInput));
+      setFirstFilter(firstFilter.filter((el) => +el[typeSelect] === +numberInput));
+      setFilterIds(filterIds + 1);
+      setAccFilters([...accFilters,
+        {
+          typeSelect,
+          operatorSelect,
+          numberInput,
+          id: filterIds + 1,
+        }]);
+      setFilterIds(filterIds + 1);
       break;
     case 'menor que':
-      setFirstFilter(previState.filter((el) => +el[typeSelect] < +numberInput));
+      setFirstFilter(firstFilter.filter((el) => +el[typeSelect] < +numberInput));
+      setFilterIds(filterIds + 1);
+      setAccFilters([...accFilters,
+        {
+          typeSelect,
+          operatorSelect,
+          numberInput,
+          id: filterIds + 1,
+        }]);
+      setFilterIds(filterIds + 1);
       break;
     default:
       break;
     }
   }
+
+  function handleMultipleFilters() {
+    if (multipleFilters.length > 0) {
+      switch (operatorSelect) {
+      case 'maior que':
+        setFirstFilter(multipleFilters.filter((el) => +el[typeSelect] > +numberInput));
+        break;
+      case 'igual a':
+        setFirstFilter(multipleFilters.filter((el) => +el[typeSelect] === +numberInput));
+        break;
+      case 'menor que':
+        setFirstFilter(multipleFilters.filter((el) => +el[typeSelect] < +numberInput));
+        break;
+      default:
+        break;
+      }
+    }
+  }
+
+  useEffect(() => {
+    setMultiplesFilters(firstFilter);
+  }, [firstFilter]);
+
+  function handleXButton({ target }, removed) {
+    const filterRemover = accFilters.filter((el) => el.id !== +target.id);
+    // const newTypesOptions = typeOptions.filter((el) => el !== removed);
+    setTypeState([...typeState, removed]);
+    setAccFilters(filterRemover);
+  }
+
+  function removeTypeOptions() {
+    const repeatedType = typeState.filter((el) => el !== typeSelect);
+    setTypeState(repeatedType);
+    setTypeSelect(repeatedType[0]);
+  }
+
+  function handleRemoveAllFilters() {
+    setAccFilters([]);
+    setTypeState(typeOptions);
+  }
+
+  useEffect(() => {
+    if (accFilters.length > 0) {
+      const reducedFilter = accFilters.reduce((acc, el) => {
+        console.log(accFilters);
+        switch (el.operatorSelect) {
+        case 'maior que':
+          return acc.filter((item) => +item[el.typeSelect] > +el.numberInput);
+        case 'igual a':
+          return acc.filter((item) => +item[el.typeSelect] === +el.numberInput);
+        case 'menor que':
+          return acc.filter((item) => +item[el.typeSelect] < +el.numberInput);
+        default:
+          return acc;
+        }
+      }, dinamicFilter);
+      setFirstFilter(reducedFilter);
+    } else {
+      setFirstFilter(dinamicFilter);
+    }
+  }, [accFilters]);
+
+  function handleNumericMultipleFilters() {
+    if (handleInitialSearch()) return;
+    handleNumericChanges();
+    handleMultipleFilters();
+    removeTypeOptions();
+  }
+
   return (
     <div>
       <select
@@ -49,31 +165,12 @@ function NumericFilter() {
         value={ typeSelect }
         onChange={ handleTypeSelect }
       >
-        <option
-          key="population"
-        >
-          population
-        </option>
-        <option
-          key="orbital_period"
-        >
-          orbital_period
-        </option>
-        <option
-          key="diameter"
-        >
-          diameter
-        </option>
-        <option
-          key="rotation_period"
-        >
-          rotation_period
-        </option>
-        <option
-          key="surface_water"
-        >
-          surface_water
-        </option>
+        { typeState.map((el) => (
+          <option
+            key={ el }
+          >
+            {el}
+          </option>)) }
       </select>
       <select
         data-testid="comparison-filter"
@@ -105,9 +202,34 @@ function NumericFilter() {
       <button
         type="button"
         data-testid="button-filter"
-        onClick={ handleNumericChanges }
+        onClick={ handleNumericMultipleFilters }
       >
         filtrar
+      </button>
+      {accFilters.map((el) => (
+        <div
+          key={ el.id }
+          data-testid="filter"
+        >
+          <p>{ el.typeSelect }</p>
+          <p>{ el.operatorSelect }</p>
+          <p>{ el.numberInput }</p>
+          <button
+            type="button"
+            name={ el.typeSelect }
+            id={ el.id }
+            onClick={ (event) => handleXButton(event, el.typeSelect) }
+          >
+            X
+          </button>
+        </div>
+      ))}
+      <button
+        data-testid="button-remove-filters"
+        type="button"
+        onClick={ handleRemoveAllFilters }
+      >
+        Remove All
       </button>
     </div>
   );
